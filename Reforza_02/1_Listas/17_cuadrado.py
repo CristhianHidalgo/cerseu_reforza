c6e14d4880fef9f3f26c4ce313edb394fe632b3c
CristhianHidalgo <hidalgoprietocristhian@gmail.com>
2024-04-22 23:48:26 -0500

    Actualización de ejercicio 17

--- Reforza_02/1_Listas/17_cuadrado.py
+++ Reforza_02/1_Listas/17_cuadrado.py
@@ -3,7 +3,7 @@
 
 lista = []
 contador = 1    # Comenzamos desde la unidad 1
-num_cuadrado = 0
+num_cuadrado = 0  # Puede ir cualquier número
 
 while contador <= 10:
     num_cuadrado = pow(contador, 2)
